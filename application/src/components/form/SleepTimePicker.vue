7e29825131c89db304264ae780494f77fe837c7e
shiiyan <36617009+shiiyan@users.noreply.github.com>
2020-05-05 18:02:01 +0900

    add sleep time picker (#6)
    * need config lint
    
    * figured out eslint
    
    * create deploy script
    
    * test travis ci
    
    * update travis yml
    
    * getting started for SSR
    
    * add express
    
    * import bootstrapVue
    
    * test bootstrap-vue
    
    * create progress bar
    
    * resolve conflict
    
    * fix progress bar and test travis ci
    
    * lint-fix
    
    * create sleeptimePicker

---
+++ application/src/components/form/SleepTimePicker.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-row align-h="center" class="mt-5">
+      <img alt="Sleeping" src="../../assets/bed.png" width="40%">
+    </b-row>
+    <b-row align-h="center" class="h2 mt-5">Input Sleeping Time</b-row>
+    <b-row align-h="center" class="mt-5">
+      <b-col sm="6">
+        <b-form-timepicker v-model="sleepTime" locale="en" placeholder="Sleep Time" now-button reset-button>
+        </b-form-timepicker>
+      </b-col>
+    </b-row>
+    <b-row align-h="center" class="h2 mb-0">~</b-row>
+    <b-row align-h="center">
+      <b-col sm="6">
+        <b-form-timepicker v-model="awakeTime" locale="en" placeholder="Awake Time" now-button reset-button>
+        </b-form-timepicker>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      sleepTime: '',
+      awakeTime: ''
+    }
+  }
+}
+</script>
