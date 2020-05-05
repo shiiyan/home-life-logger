a5644692b2b8878b1231c0bf1564f88f85edf639
shiiyan <yan39123@gmail.com>
2020-05-05 11:33:46 +0900

    import bootstrapVue

--- application/.eslintrc.js
+++ application/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     "extends": [
         "plugin:vue/essential",
         "eslint:recommended",
-        "google"
+        "standard"
     ],
     "globals": {
         "Atomics": "readonly",
