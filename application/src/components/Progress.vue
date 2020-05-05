d9b0de03a74b503112d90bb88a5e8adda4265187
shiiyan <36617009+shiiyan@users.noreply.github.com>
2020-05-05 23:12:00 +0900

    disable goBack at first step (#8)
    * need config lint
    
    * figured out eslint
    
    * create deploy script
    
    * test travis ci
    
    * update travis yml
    
    * getting started for SSR
    
    * add express
    
    * import bootstrapVue
    
    * test bootstrap-vue
    
    * create progress bar
    
    * resolve conflict
    
    * fix progress bar and test travis ci
    
    * lint-fix
    
    * create sleeptimePicker
    
    * building progress wrapper
    
    * create progress wrapper
    
    * fix conflict
    
    * add completed
    
    * disable goback at first step

--- application/src/components/Progress.vue
+++ application/src/components/Progress.vue
@@ -6,7 +6,14 @@
     </b-progress>
     <slot />
     <div class="mt-5">
-      <b-button pill variant="outline-primary" @click="goBack">Go Back</b-button>
+      <b-button
+        :disabled="currentStep===0"
+        pill
+        variant="outline-primary"
+        @click="goBack"
+      >
+        Go Back
+      </b-button>
       <b-button class="ml-3" pill variant="outline-success" @click="goNext">Go Next</b-button>
     </div>
   </div>
