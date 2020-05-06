8cf11a23c4194a6c44afc0e3b2c7f8eae8adafae
shiiyan <36617009+shiiyan@users.noreply.github.com>
2020-05-06 10:54:31 +0900

    add store (#9)
    * need config lint
    
    * figured out eslint
    
    * create deploy script
    
    * test travis ci
    
    * update travis yml
    
    * getting started for SSR
    
    * add express
    
    * import bootstrapVue
    
    * test bootstrap-vue
    
    * create progress bar
    
    * resolve conflict
    
    * fix progress bar and test travis ci
    
    * lint-fix
    
    * create sleeptimePicker
    
    * building progress wrapper
    
    * create progress wrapper
    
    * fix conflict
    
    * add completed
    
    * disable goback at first step
    
    * add store

---
+++ application/src/store.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+
+const createStore = ({ state, mutations }) => {
+  return new Vue({
+    data () {
+      return { state }
+    },
+    methods: {
+      commit (mutationName, newValue) {
+        mutations[mutationName](this.state, newValue)
+      }
+    }
+  })
+}
+
+export const store = createStore({
+  state: {
+    foodRate: 0,
+    sleepingTime: {
+      sleepTime: '',
+      awakeTIme: ''
+    },
+    workoutTime: 0
+  },
+  mutations: {
+    updateFoodRate (state, newFoodRate) {
+      state.foodRate = newFoodRate
+    },
+    updateSleepTime (state, newSleepTime) {
+      state.sleepingTime.sleepTime = newSleepTime
+    },
+    updateAwakeTime (state, newAwakeTime) {
+      state.sleepingTime.awakeTime = newAwakeTime
+    },
+    updateWorkoutTime (state, newWorkoutTime) {
+      state.workoutTime = newWorkoutTime
+    }
+  }
+
+})
