66fbcfc442de91a4b584eb5ae5be4b903f0f8962
shiiyan <yan39123@gmail.com>
2021-08-31 22:07:12 +0900

    receive test message from FCM

--- application/src/main.js
+++ application/src/main.js
@@ -12,7 +12,10 @@ const firebaseConfig = {
   authDomain: `${process.env.VUE_APP_PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${process.env.VUE_APP_PROJECT_ID}.firebaseio.com`,
   projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: `${process.env.VUE_APP_PROJECT_ID}.appspot.com`
+  storageBucket: `${process.env.VUE_APP_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 }
 
 firebase.initializeApp(firebaseConfig)
